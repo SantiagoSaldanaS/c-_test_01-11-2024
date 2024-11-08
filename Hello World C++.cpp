c1ff10f51af847d1c7a6c9e4a7ce8b4fef257e38
Santiago Saldaña Subías <84850803+SantiagoSaldanaS@users.noreply.github.com>
2024-11-08 16:47:49 -0600

    Update Hello World C++.cpp

--- Hello World C++.cpp
+++ Hello World C++.cpp
@@ -1,4 +1,10 @@
-#include <stdio.h>
+// biblioteca para usar cin y cout
+#include<iostream>
+
+// biblioteca para usar printf
+#include<stdio.h>
+
+using namespace std;
 
 int main(){
     printf("Hello World");
